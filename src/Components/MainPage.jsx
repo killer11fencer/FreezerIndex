d04e9fd54d09543a8a73d117397250ff20d946b6
Jeoffrey <jeoff.howe@gmail.com>
2019-09-15 22:54:54 -0600

    mainpage added

---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class MainPage extends Component {
+    constructor () {
+        super();
+        this.state = {
+            inventory: [],
+            admin: false
+        }
+    }
+}
